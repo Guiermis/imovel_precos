7ada4322c9c6aaff959a6e9f2fe4107a8f8735cc
Guilherme Rodrigues <62896062+Guiermis@users.noreply.github.com>
2021-11-24 10:57:31 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Imovel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora_de_Preco
+{
+    class Imovel
+    {
+        public string nome;
+        public string endereco;
+        public double preco;
+    }
+}
